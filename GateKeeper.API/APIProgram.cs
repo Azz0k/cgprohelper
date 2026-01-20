144d6d3e1cc779019be468f3f3a0d5fbea0e837f
Pavel Milokum <pavelmilokum@gmail.com>
2026-01-20 15:33:17 +0300

    add static files

--- GateKeeper.API/APIProgram.cs
+++ GateKeeper.API/APIProgram.cs
@@ -11,9 +11,11 @@ namespace GateKeeper.API
     {
         public static async Task Main(string[] args)
         {
+            string defaultConnectionString = "Data Source=AddressDatabase.sqlite";
             var settignsBuilder = new ConfigurationBuilder().AddJsonFile($"appsettings.json");
             IConfiguration config = settignsBuilder.Build();
-            var allowedOrigins = config.GetSection("AllowedOrigins").Get<string>();
+            string? allowedOrigins = config.GetSection("AllowedOrigins").Get<string>();
+            string? connectionString = config.GetSection("ConnectionString").Get<string>()??defaultConnectionString;
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
@@ -22,7 +24,7 @@ namespace GateKeeper.API
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
             builder.Services.AddOpenApi();
             builder.Services.AddScoped<DatabaseService>();
-            builder.Services.AddDbContext<AddressesDbContext>();
+            builder.Services.AddDbContext<AddressesDbContext>(options => options.UseSqlite(connectionString));
             builder.Services.AddScoped<AllowedDomainsApplication>();
             builder.Services.AddScoped<ForeingEmailsApplication>();
             builder.Services.AddScoped<LocalMonitoredEmailsApplication>();
@@ -39,6 +41,8 @@ namespace GateKeeper.API
                 });
             }
             var app = builder.Build();
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
             using var scope = app.Services.CreateScope();
             var dbService = scope.ServiceProvider.GetRequiredService<DatabaseService>();
             await dbService.InitDatabaseAsync();
